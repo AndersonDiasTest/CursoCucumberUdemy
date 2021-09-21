75b159a0c14ba50068527936dc60ece7898f287c
Anderson Dias <anderson_dias13@hotmail.com>
2021-09-20 21:04:55 -0300

    Criação do  projeto, configurações iniciais e configurações de dependencias

---
+++ src/test/java/AprendendoCucumber.feature
@@ -0,0 +1,8 @@
+# language: pt
+
+Funcionalidade: Aprendendo Cucumber
+
+Cenario: Iniciando com cucumber
+	Dado que primeiro texto
+	Quando segundo texto
+	Entao terceiro texto
---
+++ src/test/java/AprendendoCucumber.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class AprendendoCucumber {
+
+	@Dado("que primeiro texto")
+	public void que_primeiro_texto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Quando("segundo texto")
+	public void segundo_texto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Entao("terceiro texto")
+	public void terceiro_texto() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
